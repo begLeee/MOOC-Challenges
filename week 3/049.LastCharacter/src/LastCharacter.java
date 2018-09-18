211674362fd8a16fd4826ef45efaaf25a6bb1582
Amanov Begli <beg_ley@yahoo.com>
2018-09-19 00:16:19 +0300

    update
    week 3 part 1

---
+++ week 3/049.LastCharacter/src/LastCharacter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class LastCharacter {
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+
+        System.out.println("Last character: " + lastCharacter(name));
+
+    }
+
+
+    public static int length(String text) {
+        int len = text.length();
+        return len;
+    }
+
+    public static char lastCharacter(String text) {
+        char lastChar = text.charAt(length(text)-1);
+        return lastChar;
+    }
+}
